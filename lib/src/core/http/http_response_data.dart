426db5e0bd5ea6f5bd07fad3c5ece391617b2255
Anderson Mendes <anderson_mendes88@hotmail.com>
2022-10-22 03:26:42 -0300

    fix: renomeado variavel total na class ResponseData

--- lib/src/core/http/http_response_data.dart
+++ lib/src/core/http/http_response_data.dart
@@ -1,13 +1,13 @@
 abstract class ResponseData<T> {
   final T? data;
-  final int? totalItens;
+  final int? total;
   final String? errorMessage;
 
-  ResponseData({this.data, this.errorMessage, this.totalItens});
+  ResponseData({this.data, this.errorMessage, this.total});
 }
 
 class ResponseSuccess<T> extends ResponseData<T> {
-  ResponseSuccess(T data, {int? total}) : super(data: data, totalItens: total);
+  ResponseSuccess(T data, {int? total}) : super(data: data, total: total);
 }
 
 class ResponseFailure<T> extends ResponseData<T> {
