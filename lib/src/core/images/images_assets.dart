9e8c2716fc1c0cf089df86b664fea8ccc1ea3e01
Anderson Mendes <anderson_mendes88@hotmail.com>
2022-10-22 03:27:09 -0300

    feat: arquivos com as urls das imagens no assets

--- lib/src/core/images/images_assets.dart
+++ lib/src/core/images/images_assets.dart
@@ -1,1 +1,9 @@
-const logoPharmainc = 'assets/logo.png';
+const logoPharmainc = 'assets/image/logo.png';
+
+const String emptyState = 'assets/illustration/empty-state.png';
+const String resourcesNotFound = 'assets/illustration/unlock.png';
+const String handshake = 'assets/illustration/handshake-colour.png';
+const String womansuport =
+    'assets/illustration/drawkit-support-woman-colour.png';
+
+const String avatarPerson = 'assets/image/avatar-person.png';
