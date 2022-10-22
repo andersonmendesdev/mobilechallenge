2428dec3dc20cf617cbc5beef8f8c7519de6bdf7
Anderson Mendes <anderson_mendes88@hotmail.com>
2022-10-22 03:28:25 -0300

    feat: user repository implemetacao

---
+++ lib/src/data/repositories/user_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:http/http.dart';
+
+import '../../core/export.dart';
+import '../../domain/entities/user_entity.dart';
+import '../../domain/repositories/user_repository.dart';
+import '../datasource/remote/use_remote_data_source.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserRemoteDataSource _remoteDataSource;
+
+  UserRepositoryImpl({required UserRemoteDataSource remoteDataSource})
+      : _remoteDataSource = remoteDataSource;
+
+  @override
+  Future<ResponseData<List<UserEntity>>> getAll(
+      HttpRequestParameters params) async {
+    try {
+      var result = await _remoteDataSource.getAll(params);
+      return ResponseSuccess(result);
+    } on ApiException catch (e) {
+      return ResponseFailure(e.message);
+    } on FormatException {
+      return ResponseFailure(formatExeption);
+    } on SocketException {
+      return ResponseFailure(socketExceptionMessage);
+    } on ClientException {
+      return ResponseFailure(clientExceptionMessage);
+    } on HandshakeException {
+      return ResponseFailure(handshakeExceptionMessage);
+    } on DecodedException {
+      return ResponseFailure(exceptionMessage);
+    } catch (e,stack) {
+      return ResponseFailure(e.toString());
+    }
+  }
+}
